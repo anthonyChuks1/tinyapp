56cd1583ae22079d87b6b91067d21745147d928a
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-07-26 21:31:26 -0400

    added a test for the helper functions

---
+++ test/helpersTest.js
@@ -0,0 +1,33 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    // Write your assert statement here
+    assert.deepEqual(testUsers['userRandomID'], user)
+  }),
+
+  it('non-exinstent email should return undefined.', () =>{
+    const user = getUserByEmail("notindatabase@notindatabase.com", testUsers);
+    const expectedUserID = undefined;
+
+    assert.deepEqual(expectedUserID, user)
+
+  })
+});
